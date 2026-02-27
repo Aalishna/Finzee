e15ae4ad4dfff736ceeb949b8d79f5f65724cdd8
Aalishna <156046872+Aalishna@users.noreply.github.com>
2026-02-27 10:19:12 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://finzee-3bu0.onrender.com',
+  baseURL: 'https://finzee-3bu0.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
